19e863188704f39a5f5961389434b9211f9e6ffb
WandaRMaldo <wanda.rocha.maldonado@gmail.com>
2020-11-09 22:43:27 -0300

    Updates

---
+++ precache-manifest.fa80c63a64307407ad87b8beffd91066.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1890cd03eaec5f6d414270aa9f401b27",
+    "url": "/comit2/index.html"
+  },
+  {
+    "revision": "6a798209d097cc08bde3",
+    "url": "/comit2/static/css/main.cf19f59b.chunk.css"
+  },
+  {
+    "revision": "e5488a9a0fbe201ba798",
+    "url": "/comit2/static/js/2.ac04f6d9.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/comit2/static/js/2.ac04f6d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a798209d097cc08bde3",
+    "url": "/comit2/static/js/main.31556f6c.chunk.js"
+  },
+  {
+    "revision": "7e7c1e6de973487d437b",
+    "url": "/comit2/static/js/runtime-main.454e34ad.js"
+  }
+]);
